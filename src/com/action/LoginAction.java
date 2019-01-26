554d006e6b057ca3553d1c2bb38a510b56027a8b
fengyan <fengywo@163.com>
2019-01-26 10:25:28 +0800

    aaaaa

---
+++ src/com/action/LoginAction.java
@@ -0,0 +1,35 @@
+package com.action;
+
+public class LoginAction{
+	
+	private String name;
+	private String password;
+	private String change;
+	
+	
+	
+	public void login(){
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getChange() {
+		return change;
+	}
+	public void setChange(String change) {
+		this.change = change;
+	}
+	
+	
+
+}
